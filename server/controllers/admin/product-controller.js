223301a9121b789cfdbb1cdcb80ae57b3ec7012c
EngineersWorld475 <sanjaygnair777@gmail.com>
2025-01-29 08:52:39 +0530

    added comments in product controller

--- server/controllers/admin/product-controller.js
+++ server/controllers/admin/product-controller.js
@@ -2,10 +2,13 @@ const { imageUploadUtil } = require("../../helpers/cloudinary");
 
 const handleImageUpload = async (req, res) => {
     try {
+        // Converts the image buffer (stored in req.file.buffer by Multer) to a base64-encoded string.
         const b64 = Buffer.from(req.file.buffer).toString('base64');
+        // Constructs a data URL using the MIME type (req.file.mimetype) and base64-encoded data.
         const url = "data:" + req.file.mimetype + ';base64,' + b64;
+        // Calls imageUploadUtil(url) to upload the image to Cloudinary.
         const result = await imageUploadUtil(url)
-
+        // Responds with a success message and the upload result, typically containing the secure image URL and other metadata.
         res.json({
             success: true,
             result
