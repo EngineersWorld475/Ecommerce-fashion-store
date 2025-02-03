e2cff46601a3ba40ce40d4e02befc813a3badef5
EngineersWorld475 <sanjaygnair777@gmail.com>
2025-02-03 12:43:57 +0530

    user header style change

--- client/src/components/shopping-view/UserHeader.jsx
+++ client/src/components/shopping-view/UserHeader.jsx
@@ -1,12 +1,11 @@
 import { CircleUserRound, Crown, Menu, ShoppingBag } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet'
 import { Button } from '../ui/button'
 import { useDispatch, useSelector } from 'react-redux'
 import { shoppingViewHeaderMenuItems } from '../config'
-import { DropdownMenu, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '../ui/dropdown-menu'
-import { DropdownMenuContent } from '@radix-ui/react-dropdown-menu'
+import { DropdownMenu, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel } from '../ui/dropdown-menu'
 import { logoutUser } from '@/store/auth-slice'
 
 const UserHeader = () => {
@@ -18,6 +17,8 @@ const UserHeader = () => {
       navigate('/auth/login')
     })
   }
+  const [textColor, setTextColor] = useState('#60C54B')
+  const [bgColor, setBgColor] = useState('')
 
   const MenuItems = () => {
     return (
@@ -37,7 +38,7 @@ const UserHeader = () => {
 
   const HeaderRightContent = () => {
     return <>
-       <div className='hidden lg:flex flex-row justify-center items-center gap-7'>
+      <div className='hidden lg:flex flex-row justify-center items-center gap-7'>
         <ShoppingBag style={{ color: '#60C54B', width: '30px', height: '30px', cursor: 'pointer' }} />
 
         <DropdownMenu>
@@ -46,21 +47,21 @@ const UserHeader = () => {
               className='flex items-center justify-center text-center font-semibold text-lg rounded-full w-8 h-8 pb-1'
               style={{ backgroundColor: '#60C54B', color: 'white', outline: 'none' }}
             >
-              {user.username[0]}
+              {user?.username[0]}
             </div>
 
           </DropdownMenuTrigger>
           <DropdownMenuContent className='bg-white'>
-          <DropdownMenuItem ><p className="text-gray-500">Logged in as {user?.username}</p></DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <Link className="text-gray-500">Accounts</Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Button onClick={handleLogout} variant="outline" className="border border-red-500  py-3 text-red-500 hover:bg-red-600 hover:text-white ">Logout</Button>
-          </DropdownMenuItem>
+            <DropdownMenuItem ><p className="text-gray-500">Logged in as {user?.username}</p></DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link className="text-gray-500">Accounts</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Button onClick={handleLogout} variant="outline" className="border border-red-500  py-3 text-red-500 hover:bg-red-600 hover:text-white ">Logout</Button>
+            </DropdownMenuItem>
           </DropdownMenuContent>
-          
+
         </DropdownMenu>
       </div>
     </>
@@ -82,7 +83,36 @@ const UserHeader = () => {
           </SheetTrigger>
           <SheetContent className="w-full max-w-xs bg-white">
             <MenuItems />
-            <HeaderRightContent />
+            {isAuthenticated ? (
+              <>
+              <div className='flex flex-row justify-start items-center mt-7'>
+              <ShoppingBag className='text-gray-500 w-6 h-6'/>
+              <span className='text-sm ml-3 text-gray-700'>Cart</span>
+              </div>
+              <DropdownMenu>
+          <DropdownMenuTrigger className='flex flex-row items-center justify-center gap-3'>
+            <div
+              className='flex items-center justify-center text-center font-semibold text-lg rounded-full w-8 h-8 pb-1 bg-gray-700 text-white outline-none mt-7'
+            >
+              {user?.username[0]}
+            </div>
+          <p className='text-center mt-5 text-sm text-gray-700'>Logged in as {user?.username}</p>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className='bg-white'>
+            <DropdownMenuItem>
+              <Link className="text-gray-500">Accounts</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Button onClick={handleLogout} variant="outline" className="border border-red-500  py-3 text-red-500 hover:bg-red-600 hover:text-white ">Logout</Button>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+
+        </DropdownMenu>
+              </>
+
+            ) :
+              null
+            }
           </SheetContent>
         </Sheet>
         <div className='hidden lg:block'>
@@ -92,13 +122,27 @@ const UserHeader = () => {
         </div>
         {/* header right content */}
         {!isAuthenticated ? (
-        <div className='hidden lg:flex flex-row justify-center items-center gap-7'>
-        <Button onClick={() => navigate('/auth/login')} variant="outline" className="py-4 rounded-xl" style={{ color: '#60C54B' }}>Sign-in</Button>
-        <CircleUserRound style={{ color: '#60C54B', width: '30px', height: '30px', cursor: 'pointer', }} />
-      </div>
+          <div className='flex flex-row justify-center items-center gap-7'>
+            <Button onClick={() => navigate('/auth/login')} variant="outline" className="py-4 rounded-xl hover:bg-red-500 hover:text-white" onMouseEnter={() => {
+              setBgColor('#60C54B'); // green background
+              setTextColor('#FFFFFF'); // white text
+            }}
+              onMouseLeave={() => {
+                setBgColor('#FFFFFF'); // revert to default
+                setTextColor('#60C54B');
+              }}
+              style={{
+                backgroundColor: bgColor,
+                color: textColor,
+                padding: '16px',
+                borderRadius: '8px',
+                border: '1px solid #60C54B',
+              }}>Sign-in</Button>
+            <CircleUserRound style={{ color: '#60C54B', width: '30px', height: '30px', cursor: 'pointer', }} />
+          </div>
 
-        
-      ): <HeaderRightContent />
+
+        ) : <HeaderRightContent />
         }
       </div>
     </header>
